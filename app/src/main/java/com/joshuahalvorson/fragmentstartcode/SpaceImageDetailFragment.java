a462f6624dccc781dfe2ad8865b62713dc7c6587
Josh Halvorson <joshhalvorson16@gmail.com>
2019-04-08 10:14:28 -0700

    Update views with data passed from other fragment

--- app/src/main/java/com/joshuahalvorson/fragmentstartcode/SpaceImageDetailFragment.java
+++ app/src/main/java/com/joshuahalvorson/fragmentstartcode/SpaceImageDetailFragment.java
@@ -1,6 +1,8 @@
 package com.joshuahalvorson.fragmentstartcode;
 
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +10,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Space;
+import android.widget.TextView;
 
 
 /**
@@ -15,6 +20,7 @@ import android.view.ViewGroup;
  */
 public class SpaceImageDetailFragment extends Fragment {
 
+    private ImageView spaceImageView;
 
     public SpaceImageDetailFragment() {
         // Required empty public constructor
@@ -29,7 +35,38 @@ public class SpaceImageDetailFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        final SpaceImageListItem spaceImageListItem = (SpaceImageListItem) getArguments().getSerializable(MainActivity.SPACE_IMAGE_KEY);
+        spaceImageView = view.findViewById(R.id.space_image_view);
 
+        if(spaceImageListItem != null){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    final SpaceImageDetails spaceImageDetails = SpaceImageRepository.getSpaceImageDetails(spaceImageListItem.getId());
+                    new DownloadImageTask().execute(spaceImageDetails.getImageFiles().get(0).getFileUrl());
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ((TextView)view.findViewById(R.id.space_image_title)).setText(spaceImageDetails.getName());
+                            ((TextView)view.findViewById(R.id.space_image_desc)).setText(spaceImageDetails.getDescription());
+                        }
+                    });
+                }
+            }).start();
+        }
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap>{
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            Bitmap bitmap = SpaceImageRepository.getSpaceImageBitmap(strings[0]);
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            spaceImageView.setImageBitmap(bitmap);
+        }
     }
 }
