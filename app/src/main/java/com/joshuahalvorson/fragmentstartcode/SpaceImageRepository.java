73e718cf4ad3fb938af8c22ece8b89d3f1adebe5
Josh Halvorson <joshhalvorson16@gmail.com>
2019-04-08 09:35:56 -0700

    Add repo and get list method

---
+++ app/src/main/java/com/joshuahalvorson/fragmentstartcode/SpaceImageRepository.java
@@ -0,0 +1,27 @@
+package com.joshuahalvorson.fragmentstartcode;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class SpaceImageRepository {
+    public static final String GET_IMAGE_LIST_URL = "http://hubblesite.org/api/v3/images";
+    public static final String GET_IMAGE_DETAILS_URL = "http://hubblesite.org/api/v3/image/";
+
+    public static ArrayList<SpaceImageListItem> getSpaceImageListItems(){
+        String result = NetworkAdapter.httpRequest(GET_IMAGE_LIST_URL);
+        ArrayList<SpaceImageListItem> listItems = new ArrayList<>();
+        try {
+            JSONArray jsonArray = new JSONArray(result);
+            for(int i = 0; i < jsonArray.length(); i++){
+                JSONObject jsonObject = new JSONObject(jsonArray.get(i).toString());
+                listItems.add(new SpaceImageListItem(jsonObject));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return listItems;
+    }
+}
